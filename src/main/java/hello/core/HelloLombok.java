e52858f1af071ac069ffeedcff87d1e29540fe70
hainho <hainho9704@gmail.com>
2022-04-15 04:01:06 +0900

    ✨feature : 롬북 적용

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Arrays;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+       HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("name");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
